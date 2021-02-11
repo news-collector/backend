65da70c38058745521a8660cd2e9c150333a148b
Reszkettle <reszkettle@gmail.com>
2021-02-11 19:49:25 +0100

    Removed: - redundant empty constructors in child classes of Repository - __db_config from Repository because of security reasons

--- src/repositories/repository.py
+++ src/repositories/repository.py
@@ -1,3 +1,6 @@
+from mysql.connector import MySQLConnection
+from mysql.connector.cursor import MySQLCursor
+
 from src.configs import db_config
 import mysql.connector as mysql
 
@@ -5,24 +8,17 @@ import mysql.connector as mysql
 class Repository(object):
 
     def __init__(self):
-        self.__db_config = db_config.config
-        self._connection = mysql.connect(**self.__db_config)
-        self._cursor = self._connection.cursor(dictionary=True, buffered=True)
+        self._connection: MySQLConnection = mysql.connect(**db_config.config)
+        self._cursor: MySQLCursor = self._connection.cursor(dictionary=True, buffered=True)
 
 
 class WebsiteRepository(Repository):
-
-    def __init__(self):
-        super().__init__()
+    pass
 
 
 class NewsRepository(Repository):
-
-    def __init__(self):
-        super().__init__()
+    pass
 
 
 class FeedRepository(Repository):
-
-    def __init__(self):
-        super().__init__()
+    pass

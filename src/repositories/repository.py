06be004d98a969dd906ad107322c834be0ed54d8
Reszkettle <reszkettle@gmail.com>
2021-02-17 22:01:11 +0100

    Added an idea for improvement

--- src/repositories/repository.py
+++ src/repositories/repository.py
@@ -1,12 +1,14 @@
 from mysql.connector import MySQLConnection
 from mysql.connector.cursor import MySQLCursor
 from abc import abstractmethod
+
 from src.configs import db_config
 import mysql.connector as mysql
 from src.models.entities import WebsiteEntity, NewsEntity, FeedEntity
-from pypika import Table, MySQLQuery as Query
+from pypika import Table, Field, MySQLQuery as Query
 from mysql.connector.errors import Error as DBError
 import logging
+from typing import List, Dict, Type
 
 logging.getLogger().setLevel(logging.INFO)
 
@@ -15,11 +17,31 @@ news = Table('news')
 feeds = Table('rss_feeds')
 
 
+def make_fields(table: Table, column_names: List[str]) -> List[Field]:
+    columns = []
+
+    for name in column_names:
+        columns.append(table.field(name))
+
+    return columns
+
+
 class Repository(object):
 
-    def __init__(self):
+    def __init__(self, table: Table, fields: Dict[str, Field], entity: Type):
         self._connection: MySQLConnection = mysql.connect(**db_config.config)
         self._cursor: MySQLCursor = self._connection.cursor(dictionary=True, buffered=True)
+        self._table = table
+        self._fields = fields
+        self._entity = entity
+
+    def _get_by_id(self, _id: int):
+        query = str(Query.from_(self._table).select(self._table.star).where(self._fields['id'] == _id))
+        self._cursor.execute(query)
+        record = self._cursor.fetchone()
+        if record:
+            record = self._entity(**record)
+        return record
 
     @abstractmethod
     def get_by_id(self, _id: int):
@@ -44,71 +66,80 @@ class Repository(object):
 
 class WebsiteRepository(Repository):
 
+    def __init__(self):
+        fields = make_fields(websites, ['website_id', 'website_name', 'website_link'])
+        d = {'id': fields[0], 'fields': fields}
+        super().__init__(websites, d, WebsiteEntity)
+
     def get_by_id(self, _id: int):
-        query = str(Query.from_(websites).select(websites.star).where(websites.website_id == _id))
-        self._cursor.execute(query)
-        record = self._cursor.fetchone()
-        if record:
-            record = WebsiteEntity(**record)
-        logging.info(f"WebsiteRepository: get_by_id -> Record for id={_id} is {record}")
-        return record
+        return self._get_by_id(_id)
 
-    def get_all(self):
-        query = str(Query.from_(websites).select(websites.star))
-        self._cursor.execute(query)
-        records = self._cursor.fetchall()
-        if records:
-            records = [WebsiteEntity(**record) for record in records]
-        logging.info(f"WebsiteRepository: get_all -> Found {len(records)} records")
-        return records
-
-    def delete_by_id(self, _id: int) -> int:
-        query = str(Query.from_(websites).delete().where(websites.website_id == _id))
-        rows_count = 0
-        try:
-            self._cursor.execute(query)
-        except DBError as e:
-            logging.error(f"WebsiteRepository: delete_by_id -> [{e.errno}]{e.msg}")
-            self._connection.rollback()
-        else:
-            self._connection.commit()
-            rows_count = self._cursor.rowcount
-            logging.info(f"WebsiteRepository: delete_by_id -> Deleted {rows_count} record/s")
-        return rows_count
-
-    def delete_all(self) -> int:
-        query = str(Query.from_(websites).delete())
-        rows_count = 0
-        try:
-            self._cursor.execute(query)
-        except DBError as e:
-            logging.error(f"WebsiteRepository: delete_all -> [{e.errno}]{e.msg}")
-            self._connection.rollback()
-        else:
-            self._connection.commit()
-            rows_count = self._cursor.rowcount
-            logging.info(f"WebsiteRepository: delete_all -> Deleted {rows_count} record/s")
-        return rows_count
-
-    def save(self, entity: WebsiteEntity):
-        query = str(Query.into(websites).columns(websites.website_name, websites.website_link)
-                    .insert(entity.website_name, entity.website_link))
-        entity_id = None
-
-        try:
-            self._cursor.execute(query)
-        except DBError as e:
-            logging.error(f"WebsiteRepository: save -> [{e.errno}]{e.msg}")
-            self._connection.rollback()
-        else:
-            self._connection.commit()
-            entity_id = self._cursor.lastrowid
-            logging.info(f"WebsiteRepository: save -> Saved {entity} with id={entity_id}")
-
-        return entity_id
+    # def get_by_id(self, _id: int):
+    #     query = str(Query.from_(websites).select(websites.star).where(websites.website_id == _id))
+    #     self._cursor.execute(query)
+    #     record = self._cursor.fetchone()
+    #     if record:
+    #         record = WebsiteEntity(**record)
+    #     logging.info(f"WebsiteRepository: get_by_id -> Record for id={_id} is {record}")
+    #     return record
+    #
+    # def get_all(self):
+    #     query = str(Query.from_(websites).select(websites.star))
+    #     self._cursor.execute(query)
+    #     records = self._cursor.fetchall()
+    #     if records:
+    #         records = [WebsiteEntity(**record) for record in records]
+    #     logging.info(f"WebsiteRepository: get_all -> Found {len(records)} records")
+    #     return records
+    #
+    # def delete_by_id(self, _id: int) -> int:
+    #     query = str(Query.from_(websites).delete().where(websites.website_id == _id))
+    #     rows_count = 0
+    #     try:
+    #         self._cursor.execute(query)
+    #     except DBError as e:
+    #         logging.error(f"WebsiteRepository: delete_by_id -> [{e.errno}]{e.msg}")
+    #         self._connection.rollback()
+    #     else:
+    #         self._connection.commit()
+    #         rows_count = self._cursor.rowcount
+    #         logging.info(f"WebsiteRepository: delete_by_id -> Deleted {rows_count} record/s")
+    #     return rows_count
+    #
+    # def delete_all(self) -> int:
+    #     query = str(Query.from_(websites).delete())
+    #     rows_count = 0
+    #     try:
+    #         self._cursor.execute(query)
+    #     except DBError as e:
+    #         logging.error(f"WebsiteRepository: delete_all -> [{e.errno}]{e.msg}")
+    #         self._connection.rollback()
+    #     else:
+    #         self._connection.commit()
+    #         rows_count = self._cursor.rowcount
+    #         logging.info(f"WebsiteRepository: delete_all -> Deleted {rows_count} record/s")
+    #     return rows_count
+    #
+    # def save(self, entity: WebsiteEntity):
+    #     query = str(Query.into(websites).columns(websites.website_name, websites.website_link)
+    #                 .insert(entity.website_name, entity.website_link))
+    #     entity_id = None
+    #
+    #     try:
+    #         self._cursor.execute(query)
+    #     except DBError as e:
+    #         logging.error(f"WebsiteRepository: save -> [{e.errno}]{e.msg}")
+    #         self._connection.rollback()
+    #     else:
+    #         self._connection.commit()
+    #         entity_id = self._cursor.lastrowid
+    #         logging.info(f"WebsiteRepository: save -> Saved {entity} with id={entity_id}")
+    #
+    #     return entity_id
 
 
 class NewsRepository(Repository):
+
     def get_by_id(self, _id: int):
         query = str(Query.from_(news).select(news.star).where(news.news_id == _id))
         self._cursor.execute(query)

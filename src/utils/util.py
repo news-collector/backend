3939c1fffc0835ef9b3aca0bede21c48ea8ac7b2
rechu <reszkettle@gmail.com>
2021-02-27 14:57:19 +0100

    S2-0015 Removed redundant error pritning from PyDictionary using context manager workaround

--- src/utils/util.py
+++ src/utils/util.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 from PyDictionary import PyDictionary as PyDict
 
 
@@ -7,5 +10,16 @@ class SynonymsUtil(object):
         self.__py_dict = PyDict()
 
     def find_synonyms(self, word: str, synonyms_count: int = 5):
-        synonyms = self.__py_dict.synonym(word)
-        return [word] + synonyms[:synonyms_count] if synonyms is not None else [word]
+        with HiddenPrints():
+            synonyms = self.__py_dict.synonym(word)
+            return [word] + synonyms[:synonyms_count] if synonyms is not None else [word]
+
+
+class HiddenPrints:
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()
+        sys.stdout = self._original_stdout
